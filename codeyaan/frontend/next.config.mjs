5b902634c3ff54456d01d8509a45a3eeb4f625d2
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-10-08 01:32:59 +0530

    Update next.config file

--- codeyaan/frontend/next.config.mjs
+++ codeyaan/frontend/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // ✅ Static export enable करता है
+  images: {
+    unoptimized: true, // optional: Netlify पर Next.js image optimizer की जरूरत नहीं
+  },
+};
 
 export default nextConfig;
