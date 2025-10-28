dd2b437ef8a07eb6d068a3d5311aaec8d1be040f
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-10-28 23:46:03 +0530

    Backend Deploy

--- labxcode/backend/config/db.js
+++ labxcode/backend/config/db.js
@@ -3,11 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const dbPool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 });
 
 dbPool.on('connect', () => {
